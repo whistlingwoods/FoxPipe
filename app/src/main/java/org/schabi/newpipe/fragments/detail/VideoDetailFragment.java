0f102573db2d1d7458764095f934d43019363da7
whistlingwoods <72640314+whistlingwoods@users.noreply.github.com>
2025-09-14 18:23:02 +0530

    Set Maximum Lines to 5 in video title

--- app/src/main/java/org/schabi/newpipe/fragments/detail/VideoDetailFragment.java
+++ app/src/main/java/org/schabi/newpipe/fragments/detail/VideoDetailFragment.java
@@ -594,7 +594,7 @@ public final class VideoDetailFragment
                     VideoPlayerUi.DEFAULT_CONTROLS_DURATION, 180);
             binding.detailSecondaryControlPanel.setVisibility(View.VISIBLE);
         } else {
-            binding.detailVideoTitleView.setMaxLines(3);
+            binding.detailVideoTitleView.setMaxLines(5);
             animateRotation(binding.detailToggleSecondaryControlsView,
                     VideoPlayerUi.DEFAULT_CONTROLS_DURATION, 0);
             binding.detailSecondaryControlPanel.setVisibility(View.GONE);
@@ -1460,7 +1460,7 @@ public final class VideoDetailFragment
         binding.positionView.setVisibility(View.GONE);
 
         binding.detailVideoTitleView.setText(title);
-        binding.detailVideoTitleView.setMaxLines(3);
+        binding.detailVideoTitleView.setMaxLines(5);
         animate(binding.detailVideoTitleView, true, 0);
 
         binding.detailToggleSecondaryControlsView.setVisibility(View.GONE);
