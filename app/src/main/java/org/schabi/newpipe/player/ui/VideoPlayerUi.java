ef4890564af60f3acfdd6e1ce8e2865f84b66746
whistlingwoods <72640314+whistlingwoods@users.noreply.github.com>
2026-01-29 22:47:25 +0530

    Revert "Remove non-required API M version checks"
    This reverts commit b84395dea0330b0496c268070b9651873729ca8e.

--- app/src/main/java/org/schabi/newpipe/player/ui/VideoPlayerUi.java
+++ app/src/main/java/org/schabi/newpipe/player/ui/VideoPlayerUi.java
@@ -24,6 +24,7 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
@@ -1586,15 +1587,19 @@ public abstract class VideoPlayerUi extends PlayerUi implements SeekBar.OnSeekBa
             // make sure there is nothing left over from previous calls
             clearVideoSurface();
 
-            surfaceHolderCallback = new SurfaceHolderCallback(context, player.getExoPlayer());
-            binding.surfaceView.getHolder().addCallback(surfaceHolderCallback);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // >=API23
+                surfaceHolderCallback = new SurfaceHolderCallback(context, player.getExoPlayer());
+                binding.surfaceView.getHolder().addCallback(surfaceHolderCallback);
 
-            // ensure player is using an unreleased surface, which the surfaceView might not be
-            // when starting playback on background or during player switching
-            if (binding.surfaceView.getHolder().getSurface().isValid()) {
-                // initially set the surface manually otherwise
-                // onRenderedFirstFrame() will not be called
-                player.getExoPlayer().setVideoSurfaceHolder(binding.surfaceView.getHolder());
+                // ensure player is using an unreleased surface, which the surfaceView might not be
+                // when starting playback on background or during player switching
+                if (binding.surfaceView.getHolder().getSurface().isValid()) {
+                    // initially set the surface manually otherwise
+                    // onRenderedFirstFrame() will not be called
+                    player.getExoPlayer().setVideoSurfaceHolder(binding.surfaceView.getHolder());
+                }
+            } else {
+                player.getExoPlayer().setVideoSurfaceView(binding.surfaceView);
             }
 
             surfaceIsSetup = true;
@@ -1602,7 +1607,8 @@ public abstract class VideoPlayerUi extends PlayerUi implements SeekBar.OnSeekBa
     }
 
     private void clearVideoSurface() {
-        if (surfaceHolderCallback != null) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M // >=API23
+                && surfaceHolderCallback != null) {
             binding.surfaceView.getHolder().removeCallback(surfaceHolderCallback);
             surfaceHolderCallback.release();
             surfaceHolderCallback = null;
--- app/src/main/java/org/schabi/newpipe/util/PermissionHelper.java
+++ app/src/main/java/org/schabi/newpipe/util/PermissionHelper.java
@@ -12,6 +12,7 @@ import android.provider.Settings;
 import android.text.Html;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -115,6 +116,7 @@ public final class PermissionHelper {
      * @param context {@link Context}
      * @return {@link Settings#canDrawOverlays(Context)}
      **/
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public static boolean checkSystemAlertWindowPermission(final Context context) {
         if (!Settings.canDrawOverlays(context)) {
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
@@ -172,7 +174,8 @@ public final class PermissionHelper {
      * @return whether the popup is enabled
      */
     public static boolean isPopupEnabledElseAsk(final Context context) {
-        if (checkSystemAlertWindowPermission(context)) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M
+                || checkSystemAlertWindowPermission(context)) {
             return true;
         } else {
             Toast.makeText(context, R.string.msg_popup_permission, Toast.LENGTH_LONG).show();
