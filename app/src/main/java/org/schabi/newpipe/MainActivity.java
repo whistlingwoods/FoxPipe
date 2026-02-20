6007fcabccccec1a944999c004448ff70be5d612
Aayush Gupta <aayushgupta219@gmail.com>
2026-02-21 00:06:12 +0800

    Partially Migrate from Java to Kotlin
    Based on changes from refactor branch for code parity
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/org/schabi/newpipe/MainActivity.java
+++ app/src/main/java/org/schabi/newpipe/MainActivity.java
@@ -178,7 +178,7 @@ public class MainActivity extends AppCompatActivity {
             NotificationWorker.initialize(this);
         }
         if (!UpdateSettingsFragment.wasUserAskedForConsent(this)
-                && !App.getApp().isFirstRun()
+                && !App.getInstance().isFirstRun()
                 && ReleaseVersionUtil.INSTANCE.isReleaseApk()) {
             UpdateSettingsFragment.askForConsentToUpdateChecks(this);
         }
@@ -188,7 +188,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostCreate(final Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
-        final App app = App.getApp();
+        final App app = App.getInstance();
 
         if (sharedPreferences.getBoolean(app.getString(R.string.update_app_key), false)
                 && sharedPreferences
