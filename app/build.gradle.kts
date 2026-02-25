4d79cf65b5edb0761f6fd7c5df7f32290b627971
whistlingwoods <72640314+whistlingwoods@users.noreply.github.com>
2026-02-26 00:16:55 +0530

    Revert "Introduce lint configuration and enable checks"
    This reverts commit 89cb87b2a9b9f08a7224b550759e3981a4b7bc94.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,9 +88,13 @@ configure<ApplicationExtension> {
     }
 
     lint {
-        lintConfig = file("lint.xml")
-        // Continue the debug build even when errors are found
+        checkReleaseBuilds = false
+        // Or, if you prefer, you can continue to check for errors in release builds,
+        // but continue the build even when errors are found:
         abortOnError = false
+        // suppress false warning ("Resource IDs will be non-final in Android Gradle Plugin version
+        // 5.0, avoid using them in switch case statements"), which affects only library projects
+        disable += "NonConstantResourceId"
     }
 
     compileOptions {
