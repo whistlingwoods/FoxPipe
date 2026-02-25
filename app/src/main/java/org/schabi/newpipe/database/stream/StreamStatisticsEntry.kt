699cc3068380ad6031274b730a6da35d43218e4f
whistlingwoods <72640314+whistlingwoods@users.noreply.github.com>
2026-02-25 06:37:19 +0530

    Revert "Merge pull request #54 from SandipLow/feature_pr Option to hide fully watched videos from History page"
    This reverts commit db6a572c906ec55e0b1b9aa628a344ecf389bf84.

--- app/src/main/java/org/schabi/newpipe/database/stream/StreamStatisticsEntry.kt
+++ app/src/main/java/org/schabi/newpipe/database/stream/StreamStatisticsEntry.kt
@@ -5,7 +5,6 @@ import androidx.room.Embedded
 import org.schabi.newpipe.database.LocalItem
 import org.schabi.newpipe.database.history.model.StreamHistoryEntity
 import org.schabi.newpipe.database.stream.model.StreamEntity
-import org.schabi.newpipe.database.stream.model.StreamStateEntity.PLAYBACK_FINISHED_END_MILLISECONDS
 import org.schabi.newpipe.database.stream.model.StreamStateEntity.STREAM_PROGRESS_MILLIS
 import org.schabi.newpipe.extractor.stream.StreamInfoItem
 import org.schabi.newpipe.util.image.ImageStrategy
@@ -39,20 +38,6 @@ class StreamStatisticsEntry(
 
     override fun getLocalItemType(): LocalItem.LocalItemType {
         return LocalItem.LocalItemType.STATISTIC_STREAM_ITEM
-    }
-
-    /**
-     * The video will be considered as finished, if the time left is less than
-     * [PLAYBACK_FINISHED_END_MILLISECONDS] and the progress is at least 3/4 of the video length.
-     * The state will be saved anyway, so that it can be shown under stream info items, but the
-     * player will not resume if a state is considered as finished. Finished streams are also the
-     * ones that can be filtered out in the feed fragment.
-     * @return whether the stream is finished or not
-     */
-    fun isFinished(): Boolean {
-        val durationInSeconds = streamEntity.duration
-        return progressMillis >= durationInSeconds * 1000 - PLAYBACK_FINISHED_END_MILLISECONDS &&
-            progressMillis >= durationInSeconds * 1000 * 3 / 4
     }
 
     companion object {
