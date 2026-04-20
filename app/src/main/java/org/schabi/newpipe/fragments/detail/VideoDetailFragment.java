d17853aa40fb77a2213b2025eb93e7b9e4fbfeef
whistlingwoods <72640314+whistlingwoods@users.noreply.github.com>
2026-04-20 11:59:30 +0530

    Merge pull request #80 from Y2kz/fix/issue-6400-popup-video-switch-to-different-when-fullscreen
    Fix video playback when exiting popup to main screen

--- app/src/main/java/org/schabi/newpipe/fragments/detail/VideoDetailFragment.java
+++ app/src/main/java/org/schabi/newpipe/fragments/detail/VideoDetailFragment.java
@@ -205,6 +205,7 @@ public final class VideoDetailFragment
     int lastStableBottomSheetState = BottomSheetBehavior.STATE_EXPANDED;
     @State
     protected boolean autoPlayEnabled = true;
+    private boolean forceFullscreen = false;
 
     @Nullable
     private StreamInfo currentInfo = null;
@@ -867,7 +868,7 @@ public final class VideoDetailFragment
                             }
                         }
 
-                        if (isAutoplayEnabled()) {
+                        if (isAutoplayEnabled() || forceFullscreen) {
                             openVideoPlayerAutoFullscreen();
                         }
                     }
@@ -1132,7 +1133,18 @@ public final class VideoDetailFragment
      * = false}, hence preventing it from going directly fullscreen.
      */
     public void openVideoPlayerAutoFullscreen() {
-        openVideoPlayer(PlayerHelper.isStartMainPlayerFullscreenEnabled(requireContext()));
+        openVideoPlayer(forceFullscreen
+                || PlayerHelper.isStartMainPlayerFullscreenEnabled(requireContext()));
+        forceFullscreen = false;
+    }
+
+    public void setForceFullscreen(final boolean force) {
+        this.forceFullscreen = force;
+    }
+
+    @Nullable
+    public String getUrl() {
+        return url;
     }
 
     private void openNormalBackgroundPlayer(final boolean append) {
